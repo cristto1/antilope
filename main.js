3a356211b5e2c28232a70d643739eb1b2cbe0ec8
cristto1 <cristtoart1@gmail.com>
2023-07-05 19:18:21 -0500

    Preentrega JS corregida

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-let precioTotal = 0;
-let ingresePrecio = prompt('Ingrese el precio del accesorio y una vez terminado, escriba: Finalizar');
+function accesorios(){
 
-do{
-    precioTotal += parseInt(ingresePrecio);
-    ingresePrecio = prompt('Ingrese el precio del accesorio y una vez terminado, escriba: Finalizar');
-}while(ingresePrecio !== 'Finalizar')
-alert('El monto total de los accesorios que vas a llevar es de ' + precioTotal);
-    
-if(precioTotal >= 100000){
-    alert('¡Tu pedido tiene el envío gratuito!');
-}else{
-    let faltante = 100000 - precioTotal
-    alert('¡Te faltan $' + faltante + ' para que tu pedido sea gratuito!');
+    let precioTotal = 0;
+    let ingresePrecio = prompt('Ingrese el precio del accesorio o "esc" para ver el precio total');
+
+    do{
+        precioTotal += parseInt(ingresePrecio);
+        ingresePrecio = prompt('Ingrese el precio del accesorio o "esc" para ver el precio total');
+    }while(ingresePrecio !== 'esc')
+    alert('El monto total de los accesorios que vas a llevar es de ' + precioTotal);
+
+    if(precioTotal >= 100000){
+        alert('¡Tu pedido tiene el envío gratuito!');
+    }else{
+        let faltante = 100000 - precioTotal
+        alert('¡Te faltan $' + faltante + ' para que tu pedido sea gratuito!');
+    }
 }
+
+accesorios();
